eb630166f0cdf0615e576400e467da67343e73e2
huseinr2493 <52690150+huseinr2493@users.noreply.github.com>
2022-04-02 00:14:52 -0500

    Improved consistency of Course model

--- models/Course.js
+++ models/Course.js
@@ -1,24 +1,20 @@
 const HttpError = require('http-errors');
 const { db } = require('../services/database');
 const { whereParams } = require('../services/sqltools');
+const { isEmpty } = require('../services/utils');
 
 // if found return { ... }
 // if not found return {}
 // if db error, db.query will throw a rejected promise
-async function findOne(id) {
-  if (id) {
-    const { text, params } = whereParams({
-      id: id,
-    });
-
-    const res = await db.query(`SELECT * from "course" ${text} LIMIT 1;`, params);
-    if (res.rows.length > 0) {
-      return res.rows[0];
-    }
-  } else {
-    throw HttpError(400, 'Id is required.');
+async function findOne(criteria) {
+  if (!criteria || isEmpty(criteria)) {
+    throw HttpError.BadRequest('Id is required.');
   }
-
+  const { text, params } = whereParams(criteria);
+  const res = await db.query(`SELECT * from "course" ${text} LIMIT 1;`, params);
+  if (res.rows.length > 0) {
+    return res.rows[0];
+  }
   return {};
 }
 
@@ -26,9 +22,7 @@ async function findOne(id) {
 async function deleteCourse(id) {
   // check that id is not nullable
   if (id) {
-    const { text, params } = whereParams({
-      id: id,
-    });
+    const { text, params } = whereParams({ id });
 
     const res = await db.query(`DELETE FROM "course" ${text};`, params);
     if (res.rows.length > 0) {
