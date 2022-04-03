a266c45c44cefc2b2ac5905bb589d9e172c2d342
huseinr2493 <52690150+huseinr2493@users.noreply.github.com>
2022-04-02 19:33:30 -0500

    Improved code re-use, wrote tests for PUT /course endpoint

--- routes/course.test.js
+++ routes/course.test.js
@@ -1,60 +1,17 @@
-const log = require('loglevel');
-const request = require('supertest');
-const app = require('../app')();
-const Course = require('../models/Course');
+// Must be at the top. Provided by jest/tests_common
+global.jest.init();
+global.jest.init_routes();
+const { Course, auth, app, request, dataForGetCourse, dataForGetUser, samplePrivilegedUser } =
+  global.jest;
 
-beforeAll(() => {
-  log.disableAll();
-});
-
-jest.mock('../models/Course', () => {
-  return {
-    findOne: jest.fn(),
-  };
-});
-
-jest.mock('../services/environment', () => {
-  return {
-    port: 3001,
-    stytchProjectId: 'project-test-11111111-1111-1111-1111-111111111111',
-    stytchSecret: 'secret-test-111111111111',
-    masterAdminEmail: 'master@gmail.com',
-  };
-});
-
-jest.mock('../services/auth', () => {
-  const { setClearanceLevel } = jest.requireActual('../services/auth');
-
-  return {
-    authorizeSession: jest.fn().mockImplementation((req, res, next) => {
-      return next();
-    }),
-    setClearanceLevel,
-  };
-});
-
-// a helper that creates an array structure for getCourseById
-function dataForGetCourse(rows, offset = 0) {
-  const data = [];
-  for (let i = 1; i <= rows; i++) {
-    const value = i + offset;
-    data.push({
-      id: `${value}`,
-      prefix: 'DEP',
-      suffix: `${100 + i}`,
-      title: 'Introduction to Whatever',
-      description: 'Department consent required',
-      credits: 3,
-    });
-  }
-  return data;
-}
+/*
+Custom extensions defined in test_models
+- Course.resetAllMocks()
+- auth.loginAs(user, [dbUser - optional])
+*/
 
 describe('GET /course', () => {
-  beforeEach(() => {
-    Course.findOne.mockReset();
-    Course.findOne.mockResolvedValue(null);
-  });
+  beforeEach(Course.resetAllMocks);
 
   // helper functions - id is a numeric value
   async function callGetOnCourseRoute(row, key = 'id') {
@@ -106,6 +63,167 @@ describe('GET /course', () => {
       Course.findOne.mockRejectedValueOnce(new Error('Some Database Error'));
       const response = await request(app).get(`/course/100`);
       expect(response.statusCode).toBe(500);
+    });
+  });
+});
+
+describe('PUT /course', () => {
+  // TODO double check acceptance criteria
+  beforeEach(Course.resetAllMocks);
+
+  // put helper
+  async function callPutOnCourseRoute(courseId, body) {
+    const response = await request(app).put(`/course/${courseId}`).send(body);
+    return response;
+  }
+
+  describe('given an empty URL bar', () => {
+    test('should result in 400', async () => {
+      const editor = samplePrivilegedUser();
+      auth.loginAs(editor);
+
+      Course.findOne.mockResolvedValueOnce({});
+      const response = await callPutOnCourseRoute('', {}); // NO COURSE ID
+
+      expect(Course.findOne).not.toBeCalled();
+      expect(response.statusCode).toBe(400);
+      expect(response.body.error.message).toBe('Required Parameters Missing');
+    });
+  });
+
+  describe('when URL bar is non-empty', () => {
+    test('should 500 when course is not found', async () => {
+      const editor = samplePrivilegedUser();
+      auth.loginAs(editor, {}); // NO EDITOR IN DB
+
+      const course = dataForGetCourse(1)[0];
+
+      const desiredChanges = {
+        prefix: 'ANTH',
+        suffix: '220HON',
+        title: 'Cultural Anthropology',
+      };
+
+      Course.findOne.mockResolvedValueOnce(course);
+      Course.edit.mockResolvedValueOnce(Object.assign(course, desiredChanges));
+
+      const response = await callPutOnCourseRoute(course.id, desiredChanges);
+
+      expect(response.statusCode).toBe(500);
+      expect(response.body.error.message).toBe('Your account is not found in the database!');
+    });
+
+    test('should 401 when not authorized to edit courses', async () => {
+      const editor = dataForGetUser(1)[0];
+      editor.enable = 'true';
+      auth.loginAs(editor); // Unprivileged editor
+
+      const course = dataForGetCourse(1)[0];
+
+      const desiredChanges = {
+        prefix: 'ANTH',
+        suffix: '220HON',
+        title: 'Cultural Anthropology',
+      };
+
+      Course.findOne.mockResolvedValueOnce(course);
+      Course.edit.mockResolvedValueOnce(Object.assign(course, desiredChanges));
+
+      const response = await callPutOnCourseRoute(course.id, desiredChanges);
+
+      expect(response.statusCode).toBe(401);
+      expect(response.body.error.message).toBe('You are not allowed to do that!');
+    });
+
+    test('should 404 when the course is not found', async () => {
+      const editor = samplePrivilegedUser();
+      auth.loginAs(editor);
+
+      const desiredChanges = {
+        prefix: 'ANTH',
+        suffix: '220HON',
+        title: 'Cultural Anthropology',
+      };
+
+      Course.findOne.mockResolvedValueOnce({}); // NO COURSE
+      Course.edit.mockResolvedValueOnce({});
+
+      const response = await callPutOnCourseRoute(1, desiredChanges);
+      expect(response.statusCode).toBe(404);
+      expect(Course.edit).not.toBeCalled();
+      expect(response.body.error.message).toBe('Not Found');
+    });
+
+    test('should respond 200 and successfully return edited version of user', async () => {
+      const editor = samplePrivilegedUser();
+      auth.loginAs(editor);
+
+      const course = dataForGetCourse(1)[0];
+
+      const desiredChanges = {
+        prefix: 'ANTH',
+        suffix: '220HON',
+        title: 'Cultural Anthropology',
+      };
+
+      Course.findOne.mockResolvedValueOnce(course);
+      const expectedReturn = Object.assign(course, desiredChanges);
+      Course.edit.mockResolvedValueOnce(desiredChanges);
+
+      const response = await callPutOnCourseRoute(course.id, desiredChanges);
+
+      expect(response.statusCode).toBe(200);
+      // check all properties
+      for (const key of Object.keys(response.body)) {
+        expect(response.body).toHaveProperty(key, expectedReturn[key]);
+      }
+    });
+
+    test('should still work even if no body parameters are specified', async () => {
+      const editor = samplePrivilegedUser();
+      auth.loginAs(editor);
+
+      const course = dataForGetCourse(1)[0];
+
+      const desiredChanges = {};
+
+      Course.findOne.mockResolvedValueOnce(course);
+      const expectedReturn = Object.assign(course, desiredChanges);
+      Course.edit.mockResolvedValueOnce(desiredChanges);
+
+      const response = await callPutOnCourseRoute(course.id, desiredChanges);
+
+      expect(response.statusCode).toBe(200);
+      // check all properties
+      for (const key of Object.keys(response.body)) {
+        expect(response.body).toHaveProperty(key, expectedReturn[key]);
+      }
+    });
+
+    test("should still work if the user's role is Director", async () => {
+      const editor = samplePrivilegedUser();
+      editor.role = 'director';
+      auth.loginAs(editor);
+
+      const course = dataForGetCourse(1)[0];
+
+      const desiredChanges = {
+        prefix: 'ANTH',
+        suffix: '220HON',
+        title: 'Cultural Anthropology',
+      };
+
+      Course.findOne.mockResolvedValueOnce(course);
+      const expectedReturn = Object.assign(course, desiredChanges);
+      Course.edit.mockResolvedValueOnce(desiredChanges);
+
+      const response = await callPutOnCourseRoute(course.id, desiredChanges);
+
+      expect(response.statusCode).toBe(200);
+      // check all properties
+      for (const key of Object.keys(response.body)) {
+        expect(response.body).toHaveProperty(key, expectedReturn[key]);
+      }
     });
   });
 });
