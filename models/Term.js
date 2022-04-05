3e9b464096a5bdd2fcf98d237c2a95ed728f4df7
joshua.elmer <77998341+joshua-elmer@users.noreply.github.com>
2022-04-05 11:52:13 -0500

    Standardized findOne, added findAll

--- models/Term.js
+++ models/Term.js
@@ -1,21 +1,36 @@
 const HttpError = require('http-errors');
+const log = require('loglevel');
 const { db } = require('../services/database');
 const { whereParams, insertValues, updateValues } = require('../services/sqltools');
-// const { whereParams } = require('../services/sqltools');
+const { isEmpty, isObject, isString, isNumber } = require('../services/utils');
 
-async function get(id) {
-  if (id) {
-    const res = await db.query(`SELECT * FROM "term" WHERE id = ${id};`);
-    if (res.rows.length > 0) {
-      return res.rows[0];
-    }
-    else {
-      throw HttpError(500, 'Some Error Occurred');
-      return {};
-    }
-  } else {
-    throw HttpError(400, 'Id is required.');
+// if found return { ... }
+// if not found return {}
+// if db error, db.query will throw a rejected promise
+async function findOne(criteria) {
+  if (!criteria || isEmpty(criteria)) {
+    throw HttpError.BadRequest('Id is required.');
   }
+  const { text, params } = whereParams(criteria);
+  const res = await db.query(`SELECT * from "term" ${text} LIMIT 1;`, params);
+  if (res.rows.length > 0) {
+    return res.rows[0];
+  }
+  return {};
+}
+
+// if found return [ {}, {} ... ]
+// if not found return []
+// if db error, db.query will throw a rejected promise
+async function findAll(criteria, limit = 100, offset = 0) {
+  const { text, params } = whereParams(criteria);
+  const n = params.length;
+  const p = params.concat([limit, offset]);
+  const res = await db.query(`SELECT * from "term" ${text} LIMIT $${n + 1} OFFSET $${n + 2};`, p);
+  log.debug(
+    `Retrieved ${res.rows.length} terms from db with criteria: ${text}, ${JSON.stringify(params)}`
+  );
+  return res.rows;
 }
 
 async function count() {
@@ -29,5 +44,7 @@ async function count() {
 }
 
 module.exports = {
+  findOne,
+  findAll,
   count,
 };
