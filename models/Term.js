c9df5feda0658b363279df37895bb29e81cdfb1d
hstuewer <90868148+hstuewer@users.noreply.github.com>
2022-04-05 11:26:41 -0500

    Added get function to Term.js

--- models/Term.js
+++ models/Term.js
@@ -1,6 +1,22 @@
 const HttpError = require('http-errors');
 const { db } = require('../services/database');
+const { whereParams, insertValues, updateValues } = require('../services/sqltools');
 // const { whereParams } = require('../services/sqltools');
+
+async function get(id) {
+  if (id) {
+    const res = await db.query(`SELECT * FROM "term" WHERE id = ${id};`);
+    if (res.rows.length > 0) {
+      return res.rows[0];
+    }
+    else {
+      throw HttpError(500, 'Some Error Occurred');
+      return {};
+    }
+  } else {
+    throw HttpError(400, 'Id is required.');
+  }
+}
 
 async function count() {
   const res = await db.query(`SELECT COUNT(*) FROM "term"`);
