23182ee43d4ebe829b5d97f313c555edae11ef0f
joshua.elmer <77998341+joshua-elmer@users.noreply.github.com>
2022-04-05 12:08:22 -0500

    Added findOne unit tests

--- models/Term.test.js
+++ models/Term.test.js
@@ -1,22 +1,53 @@
-global.jest.init(false);
+global.jest.init(false); // Init without models
 global.jest.init_db();
+const { dataForGetTerm, db } = global.jest;
 
-const { db } = global.jest;
 const Term = require('./Term');
 
-describe('Course Model', () => {
+describe('Term Model', () => {
   beforeEach(() => {
     db.query.mockReset();
     db.query.mockResolvedValue(null);
   });
-  describe('Count Courses', () => {
-    test('One Course in the Database', async () => {
-      db.query.mockResolvedValue({ rows: [{ count: 1 }] });
-      const res = await Term.count();
+
+  describe('querying a single term by id', () => {
+    test('confirm calls to query', async () => {
+      const data = dataForGetTerm(1);
+      const id = data[0].id;
+
+      db.query.mockResolvedValue({ rows: data });
+      await Term.findOne({ id });
+
       expect(db.query.mock.calls).toHaveLength(1);
-      expect(db.query.mock.calls[0]).toHaveLength(1);
-      expect(db.query.mock.calls[0][0]).toBe(`SELECT COUNT(*) FROM "term"`);
-      expect(res).toHaveProperty('count', 1);
+      expect(db.query.mock.calls[0][1][0]).toBe(id);
+    });
+
+    test('should return a single term', async () => {
+      const row = dataForGetTerm(1)[0];
+      const id = row.id;
+
+      db.query.mockResolvedValue({ rows: [row] });
+      const term = await Term.findOne({ id });
+
+      for (const key in Object.keys(row)) {
+        expect(term).toHaveProperty(key, row[key]);
+      }
+    });
+
+    test('should return empty for unfound term', async () => {
+      db.query.mockResolvedValue({ rows: [] });
+      const term = await Term.findOne({ id: 123 });
+
+      expect(Object.keys(term)).toHaveLength(0);
+    });
+
+    test('should throw error for database error', async () => {
+      db.query.mockRejectedValueOnce(new Error('a testing database error'));
+      await expect(Term.findOne({ id: 123 })).rejects.toThrowError('a testing database error');
+    });
+
+    test('should throw error if no parameters', async () => {
+      await expect(Term.findOne()).rejects.toThrowError('Id is required.');
     });
   });
 });
