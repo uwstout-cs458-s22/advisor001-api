e54b11caab4fdf9c15f7ec3f5090125503ec1f33
Erik Drake <drakee5596>
2022-04-21 08:26:08 -0500

    Base Edit
    Base Edit

--- models/Program.js
+++ models/Program.js
@@ -1,8 +1,8 @@
 const HttpError = require('http-errors');
 const log = require('loglevel');
 const { db } = require('../services/database');
-const { whereParams } = require('../services/sqltools');
-const { isEmpty } = require('../services/utils');
+const { whereParams, updateValues } = require('../services/sqltools');
+const { isEmpty, isObject } = require('../services/utils');
 
 // if found return { ... }
 // if not found return {}
@@ -26,14 +26,36 @@ async function findAll(criteria, limit = 100, offset = 0) {
   const { text, params } = whereParams(criteria);
   const n = params.length;
   const p = params.concat([limit, offset]);
-  const res = await db.query(`SELECT * from "program" ${text} LIMIT $${n + 1} OFFSET $${n + 2};`, p);
+  const res = await db.query(
+    `SELECT * from "program" ${text} LIMIT $${n + 1} OFFSET $${n + 2};`,
+    p
+  );
   log.debug(
-    `Retrieved ${res.rows.length} programs from db with criteria: ${text}, ${JSON.stringify(params)}`
+    `Retrieved ${res.rows.length} programs from db with criteria: ${text}, ${JSON.stringify(
+      params
+    )}`
   );
   return res.rows;
 }
 
+async function edit(id, newValues) {
+  // TODO the routes should probably be doing the validation, not this
+  if (id && newValues && isObject(newValues)) {
+    const { text, params } = updateValues({ id }, newValues);
+    const res = await db.query(`UPDATE "term" ${text} RETURNING *;`, params);
+    // did it work?
+    if (res.rows.length > 0) {
+      return res.rows[0];
+    }
+    // nothing was updated
+    return {};
+  }
+  // TODO ambiguous error
+  else throw HttpError.BadRequest('Id is required.');
+}
+
 module.exports = {
-    findOne,
-    findAll,
+  findOne,
+  findAll,
+  edit,
 };
--- routes/program.js
+++ routes/program.js
@@ -3,39 +3,62 @@ const log = require('loglevel');
 const HttpError = require('http-errors');
 const { isEmpty } = require('./../services/utils');
 const Program = require('./../models/Program');
-const { authorizeSession } = require('./../services/auth');
+const { authorizeSession, setClearanceLevel } = require('./../services/auth');
 
 module.exports = () => {
-    const router = express.Router();
-  
-    // Get multiple programs
-    router.get('/', authorizeSession, async (req, res, next) => {
-      try {
-        const programs = await Program.findAll(null, req.query.limit, req.query.offset);
-        log.info(`${req.method} ${req.originalUrl} success: returning ${programs.length} program(s)`);
-        return res.send(programs);
-      } catch (error) {
-        next(error);
+  const router = express.Router();
+
+  // Get multiple programs
+  router.get('/', authorizeSession, async (req, res, next) => {
+    try {
+      const programs = await Program.findAll(null, req.query.limit, req.query.offset);
+      log.info(`${req.method} ${req.originalUrl} success: returning ${programs.length} program(s)`);
+      return res.send(programs);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  // Get single program
+  router.get('/:programId', authorizeSession, async (req, res, next) => {
+    try {
+      const programId = req.params.programId;
+      const program = await Program.findOne({ id: programId });
+      // catch if the program does not exist
+      if (isEmpty(program)) {
+        throw new HttpError.NotFound();
       }
-    });
-  
-    // Get single program
-    router.get('/:programId', authorizeSession, async (req, res, next) => {
-      try {
-        const programId = req.params.programId;
-        const program = await Program.findOne({ id: programId });
-        // catch if the program does not exist
-        if (isEmpty(program)) {
-          throw new HttpError.NotFound();
-        }
-        log.info(`${req.method} ${req.originalUrl} success: returning program ${programId}`);
-        return res.send(program);
-        // catch general errors
-      } catch (error) {
-        next(error);
+      log.info(`${req.method} ${req.originalUrl} success: returning program ${programId}`);
+      return res.send(program);
+      // catch general errors
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put('/:id?', authorizeSession, setClearanceLevel('director'), async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      if (!id || id === '') {
+        throw HttpError(400, 'Required Parameters Missing');
       }
-    });
-  
-    return router;
-  };
-  
+
+      const program = await Program.findOne({ id });
+
+      if (isEmpty(program)) {
+        throw new HttpError.NotFound();
+      }
+
+      const editResult = await Program.edit(id, {
+        title: req.body.title || program.title,
+        description: req.body.description || program.description,
+      });
+      log.info(`${req.method} ${req.originalUrl} success: returning edited course ${editResult}`);
+      return res.send(editResult);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  return router;
+};
